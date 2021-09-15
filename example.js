b217c9773a3d185c9d27672269bf8e0e1ca7f04e
Micha Okkerman <micha.okkerman@helloprint.com>
2021-09-15 10:09:14 +0200

    add some extra example code

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ const validator = new Validator()
 
 validator.setSchema(cars)
 
-const validationResult = validator.validate({
+const validationResultIncorrect = validator.validate({
     type: 'Astra',
     brand: 'Opel',
     abc: Math.PI,
@@ -14,4 +14,21 @@ const validationResult = validator.validate({
     ob: []
 })
 
-console.log(validationResult.isValid(), validationResult)
+console.log('Validating an incorrect payload:')
+console.log(validationResultIncorrect.isValid(), validationResultIncorrect)
+console.log('----')
+
+
+const validationResultCorrect = validator.validate({
+    type: 'Astra',
+    brand: 'Opel',
+    abc: Math.PI,
+    milage: 199999.99,
+    extras: [],
+    ob: []
+})
+
+console.log('Validating a correct payload:')
+console.log(validationResultCorrect.isValid(), validationResultCorrect)
+console.log('----')
+console.log('Done')
